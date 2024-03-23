1c1cbe2f8a1d92b8b4feb6d7fe7618fbeb792d83
TetianaDiachenko <tetianamyhun@gmail.com>
2024-03-23 22:01:12 +0200

    added scroll on click on the menu items in header

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,7 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 // Popup
-
 const popupLinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll('.lock-padding');
 
 let unlock = true;
 
@@ -50,8 +47,29 @@ function popupClose(popupActive) {
 	}
 }
 
-// Swiper slider
+// Scroll to selected section
+function onLinkClick(e) {
+	const link = e.target;
+	if(link.dataset.goto && document.querySelector(link.dataset.goto)) {
+		const gotoBlock = document.querySelector(link.dataset.goto);
+		const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
 
+		window.scrollTo({
+			top: gotoBlockValue,
+			behavior: "smooth"
+		});
+		e.preventDefault();
+	}	
+}
+//Scroll on click scrollebleLinks
+const scrollebleLinks = document.querySelectorAll('a[data-goto]');
+if(scrollebleLinks.length > 0) {
+	scrollebleLinks.forEach(scrollebleLink => {
+		scrollebleLink.addEventListener("click", onLinkClick);
+	});
+}
+
+// Swiper slider
 const swiper = new Swiper('.swiper', {
 	loop: true,
 	speed: 400,
